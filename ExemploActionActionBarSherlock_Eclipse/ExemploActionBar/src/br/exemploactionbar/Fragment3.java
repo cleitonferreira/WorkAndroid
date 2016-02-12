d64e5ab3740976def29204683e00891ee264d2fc
Cleiton Ferreira <cleitonferreiraa@hotmail.com>
2016-02-12 20:56:33 -0200

    Criando Suporte ActionBar Android Com ActionBarSherlock

---
+++ ExemploActionActionBarSherlock_Eclipse/ExemploActionBar/src/br/exemploactionbar/Fragment3.java
@@ -0,0 +1,19 @@
+package br.exemploactionbar;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Fragment3 extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		TextView tv = new TextView(getActivity());
+		tv.setText("Fragment 3 em ação");
+		
+		return(tv);
+	}
+}
