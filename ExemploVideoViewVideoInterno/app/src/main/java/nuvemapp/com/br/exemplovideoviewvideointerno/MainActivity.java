c505b53d6615a0fc78749ee86e57939d0ffaba3e
Cleiton Ferreira <cleitonferreiraa@hotmail.com>
2016-02-14 14:40:19 -0200

    VideoView Com Vídeo Interno a APP Android

---
+++ ExemploVideoViewVideoInterno/app/src/main/java/nuvemapp/com/br/exemplovideoviewvideointerno/MainActivity.java
@@ -0,0 +1,42 @@
+package nuvemapp.com.br.exemplovideoviewvideointerno;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        File sdcard = android.os.Environment.getExternalStorageDirectory();
+        //String path = sdcard.toString();
+        //Log.i("Script", path);
+        File file = new File(sdcard, "video.mp4");
+
+        //VideoView video = new VideoView(this);
+        VideoView video = (VideoView) findViewById(R.id.videoView);
+
+        //LinearLayout ll = (LinearLayout) findViewById(R.id.linearLayout);
+        //ll.addView(video);
+
+        //String src = file.getAbsolutePath();
+        //video.setVideoPath(src);
+
+        //Uri src = Uri.parse("http://www.villopim.com.br/android/video.3gp");
+        Uri src = Uri.parse("android.resource://nuvemapp.com.br.exemplovideoviewvideointerno/raw/video");
+        video.setVideoURI(src);
+
+        video.setMediaController(new MediaController(this));
+
+
+    }
+}
