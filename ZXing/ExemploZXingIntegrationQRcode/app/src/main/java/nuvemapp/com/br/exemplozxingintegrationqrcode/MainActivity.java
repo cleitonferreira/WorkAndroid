58b4cab2a3c262dc808c299d6b463124cefa4a3e
Cleiton Ferreira <cleitonferreiraa@hotmail.com>
2016-05-03 15:21:35 -0300

    Integrando o QRCode ZXing no Android

---
+++ ZXing/ExemploZXingIntegrationQRcode/app/src/main/java/nuvemapp/com/br/exemplozxingintegrationqrcode/MainActivity.java
@@ -0,0 +1,94 @@
+package nuvemapp.com.br.exemplozxingintegrationqrcode;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+public class MainActivity extends Activity {
+    ImageView qrCodeImageview;
+    String QRcode;
+    public final static int WIDTH=500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getID();
+// create thread to avoid ANR Exception
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+// this is the msg which will be encode in QRcode
+                QRcode="Este e o meu primeiro codigo QR";
+
+
+
+                try {
+                    synchronized (this) {
+                        wait(5000);
+// runOnUiThread method used to do UI task in main thread.
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    Bitmap bitmap = null;
+
+                                    bitmap = encodeAsBitmap(QRcode);
+                                    qrCodeImageview.setImageBitmap(bitmap);
+
+                                } catch (WriterException e) {
+                                    e.printStackTrace();
+                                } // end of catch block
+
+                            } // end of run method
+                        });
+
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+
+            }
+        });
+        t.start();
+
+    }
+
+    private void getID() {
+        qrCodeImageview=(ImageView) findViewById(R.id.img_qr_code_image);
+    }
+
+    // this is method call from on create and return bitmap image of QRCode.
+    Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, WIDTH, WIDTH, null);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+        int w = result.getWidth();
+        int h = result.getHeight();
+        int[] pixels = new int[w * h];
+        for (int y = 0; y < h; y++) {
+            int offset = y * w;
+            for (int x = 0; x < w; x++) {
+                pixels[offset + x] = result.get(x, y) ? getResources().getColor(R.color.black):getResources().getColor(R.color.white);
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, 500, 0, 0, w, h);
+        return bitmap;
+    } /// end of this method
+
+
+}
